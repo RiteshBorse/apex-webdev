b403c718637274216c00f34acc502c4e26342c5c
RiteshBorse <riteshborse008@gmail.com>
2024-03-25 00:36:06 +0530

    apartmenti id generated

---
+++ scripts/utlis/apartid.js
@@ -0,0 +1,35 @@
+let detail = JSON.parse(localStorage.getItem('loggeduserdata'));
+export function generateApartId()
+{
+    let id;
+    id = detail.country.charAt(0).toUpperCase();
+    id += detail.state.charAt(0).toUpperCase();
+    id += detail.district.charAt(0).toUpperCase();
+    id += detail.apartmentName.charAt(0).toUpperCase();
+    id += detail.apartmentName.slice(-1).toUpperCase();
+    if(detail.flats > 99)
+    {
+        id += 99;
+    }
+    else if ( detail.flats < 10)
+    {
+        id += '0' + detail.flats;
+    }
+    else {
+        id += detail.flats;
+    }
+
+    if (detail.shops > 99) {
+        id += 99;
+    } else if (detail.shops < 10) {
+        id += '0' + detail.shops;
+    } else {
+        id += detail.shops;
+    }
+
+    id += detail.pincode.slice(-1);
+    
+   
+    return id;
+
+}
