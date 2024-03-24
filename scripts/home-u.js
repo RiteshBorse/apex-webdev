6d258660af21d4ad5ed20215ac0f42cd9ca627dc
RiteshBorse <riteshborse008@gmail.com>
2024-03-24 16:09:18 +0530

    containarization

--- scripts/home-u.js
+++ scripts/home-u.js
@@ -1,14 +1,14 @@
 import { search } from '../scripts/utlis/fuse.js'
 document.querySelector('.js-document-wallet')
     .addEventListener(('click'), () => {
-        document.querySelector('.js-content')
+        document.querySelector('.js-features')
             .innerHTML = `
                 <p style=" color : red ; font-size : 45px; "> Document Wallet will be loaded Here</p>
             `;
     });
 document.querySelector('.js-complaint-track')
     .addEventListener(('click'), () => {
-        document.querySelector('.js-content')
+        document.querySelector('.js-features')
             .innerHTML = `
                 <p style=" color : green ; font-size : 45px; "> Complaint Tracking will be loaded Here</p>
             `;
@@ -17,7 +17,7 @@ document.querySelector('.js-complaint-track')
 
 document.querySelector('.js-ammenites-booking')
     .addEventListener(('click'), () => {
-        document.querySelector('.js-content')
+        document.querySelector('.js-features')
             .innerHTML = `
                 <p style=" color : blue ; font-size : 45px; "> Ammenites booking will be loaded Here</p>
             `;
@@ -30,7 +30,7 @@ document.querySelector('.js-search-icon')
         const item = obj.item.fun;
         console.log(`.js-${item}`);
     
-        document.querySelector('.js-content')
+        document.querySelector('.js-features')
             .innerHTML = `
                 <p style=" color : blue ; font-size : 45px; "> ${item} will be loaded Here</p>
             `;
